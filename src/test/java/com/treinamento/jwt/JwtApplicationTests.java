a91705356b0e420f272c368da87b9886e03883ae
Misael Lima <misaellimadossantos@hotmail.com>
2021-10-15 10:14:29 -0400

    Iniciando repositorio

---
+++ src/test/java/com/treinamento/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
